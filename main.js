2bf74232c4a8828ddb3160cca01080e2890efc5d
Thomas GODFRIN <thomas.godfrin@stagiairesmns.fr>
2026-02-19 16:53:20 +0100

    creation mot aleatoire+ ecouter clavier +egexp

---
+++ main.js
@@ -0,0 +1,58 @@
+const mots = [
+    "Soleil",
+    "Montagne",
+    "Liberté",
+    "Océan",
+    "Forêt",
+    "Voyage",
+    "Musique",
+    "Étoile",
+    "Courage",
+    "Bonheur",
+    "Rivière",
+    "Aventure",
+    "Nuage",
+    "Horizon",
+    "Passion",
+    "Tempête",
+    "Silence",
+    "Lumière",
+    "Mystère",
+    "Espoir"
+];
+
+const boutonNewPartie = document.querySelector(".btn-primary")
+
+boutonNewPartie.addEventListener("click", () => {
+    let motRecherche
+
+    function randomMots() {
+        let nbRandom = Math.floor(Math.random() * 20);
+        console.log(nbRandom)
+        motRecherche = mots[nbRandom]
+    }
+
+    randomMots()
+    console.log(motRecherche)
+    console.log(`${motRecherche.length}`)
+
+    const wordDisplay = document.querySelector('#word-display')
+    const newSpan = "<span class=\"letter-placeholder\" > </span>"
+    wordDisplay.innerHTML = newSpan
+    for (let i = 0; i < motRecherche.length - 1; i++) {
+        wordDisplay.insertAdjacentHTML("beforeend", "<span class=\"letter-placeholder\" > </span>")
+    }
+})
+
+let lettreAppuyer = document.addEventListener("keydown",(event)=>{
+    console.log(event.key)
+    if (event.key.match("[a-z]")) {
+        
+    }else{
+        window.alert("Lettre non reconnue")
+    }
+    
+    
+})
+
+
